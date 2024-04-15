539fade1581dc6c83a4c641e91bf2ca8364feef8
MidnightCurator <andrewlovitt53@gmail.com>
2024-04-15 17:28:50 -0500

    test

---
+++ js/test_functions.js
@@ -0,0 +1,11 @@
+function funcOne() {
+    document.getElementById("p1").innerHTML = "hell yeah";
+}
+
+function openSideNav() {
+    document.getElementById("mySidenav").style.width = "16em";
+}
+
+function closeSideNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
